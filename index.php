a61f4f83fa969c15b9a36f039b70ab1e742da3b7
jeiso <jcastroladino@gmail.com>
2017-04-06 19:53:55 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>Funciones</title>
+</head>
+<body>
+
+	<br>
+	<br>
+	<br>
+	<center>
+	<label><b><h1>Ingrese una funci&oacute;n</h1></b></label>
+
+	<form  action="detectar_si_existe_mysqli.php" method="get">
+
+		<input type="text" name="funcion" placeholder="ingrese la funci&oacute;n">
+
+		<input type="submit" value="crear">
+		
+	</form> 
+	</center>
+
+</body>
+</html>
